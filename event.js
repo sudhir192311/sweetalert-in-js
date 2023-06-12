c724fc8eb6c713c2d5a79b4be3180d5bf5fbff99
Sudhir swain <70561891+sudhir192311@users.noreply.github.com>
2023-06-12 23:37:09 +0530

    first  commit

---
+++ event.js
@@ -0,0 +1,16 @@
+
+window.addEventListener("load", function(){
+});
+
+function sudhir(){
+    swal({
+        title: "Good job!",
+        text: "You clicked the button!",
+        icon: "success",
+        button: "ok",
+      });
+
+
+}
+
+
